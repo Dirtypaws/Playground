d2f0ba4f869ca8e333bdd52dcbd5834201e1c3c9
Krizanac, Matt <matt.krizanac@prcm.com>
2016-09-20 11:59:49 -0500

    Added MySql.EF to Playground solution

---
+++ CLIENTS/Playground/MySql.EF/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace MySql.EF
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            var builder = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("SampleConnection");
+
+            // Create an employee instance and save the entity to the database
+            using (var context = EmployeesContext.EmployeesContextFactory.Create(connectionString))
+            {
+                var actor = context.Actors.FirstOrDefault();
+                Console.WriteLine($"Retrieved actor with Id: {actor.actor_id}");
+            }
+
+            
+        }
+    }
+}
